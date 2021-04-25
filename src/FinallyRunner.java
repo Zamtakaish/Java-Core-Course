3554fb7c37b1314fca438d7d1e6d480b45e1a46f
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-04-25 22:11:21 +0300

    feat: complete try-catch-finally task

---
+++ src/FinallyRunner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FinallyRunner {
+    public static void main(String[] args) {
+
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(System.in);
+
+            int[] numbers = {12, 2, 3, 4};
+
+            int number = numbers[5];
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("Before Scanner Close");
+
+            if(scanner!=null){
+                scanner.close();
+            }
+        }
+
+        System.out.println("Just before closing out main");
+;    }
+}
