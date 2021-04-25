4a75f71fe65fe3afb001493eaa3f81ae909401b0
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-04-25 22:11:51 +0300

    feat: complete checked\unchecked exceptions task

---
+++ src/CheckedExeptionRunner.java
@@ -0,0 +1,19 @@
+public class CheckedExeptionRunner {
+    public static void main(String[] args) {
+        try{
+            someOtherMethod1();
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        someOtherMethod2();
+    }
+
+    private static void someOtherMethod2() throws RuntimeException {
+    }
+
+    private static void someOtherMethod1() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+}
