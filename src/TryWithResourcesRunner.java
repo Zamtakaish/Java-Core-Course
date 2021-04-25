5edfcce344cfd5cc793aad357fbde6ff2c1ea4fa
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-04-25 22:56:04 +0300

    feat: refactor try-catch-finally constriction to try with resourses

---
+++ src/TryWithResourcesRunner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TryWithResourcesRunner {
+    public static void main(String[] args) {
+
+        //This block is automatically calls close() method on the Scanner variable
+        // when exception is occurred (Java 7 feature).
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            int[] numbers = {12, 2, 3, 4};
+
+            int number = numbers[21];
+        }
+;    }
+}
